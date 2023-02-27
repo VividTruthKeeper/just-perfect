55a1353d03174b56df45e7e02f617234783e6d4f
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-27 18:35:45 +0500

    delete and reinstall

--- server/src/functions/hashPassword.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-const saltRounds = 10;
-
-export const hashPassword = async (plainPassword: string): Promise<string> => {
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hash = await bcrypt.hash(plainPassword, salt);
-  return hash;
-};
